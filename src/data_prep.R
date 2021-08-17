c1ad9efa4c25cbdb7860420df54e8da49cf2bd52
Jared Joseph <jared.n.joseph@gmail.com>
2021-08-16 20:03:14 -0700

    converted starting data to network

--- src/data_prep.R
+++ src/data_prep.R
@@ -4,7 +4,7 @@
 
 # data in/out ####
 
-bis = read.csv("data/LG Network Map - Current Draft(1).csv", header = TRUE, stringsAsFactors = FALSE)
+bis = read.csv("data/LG Network Map - Current Draft(1).csv", header = TRUE, stringsAsFactors = FALSE)[-1,]
 
 # data prep ####
 
@@ -22,8 +22,12 @@ nodes = data.frame("id" = nodes$Id,
 # split edges off
 edges = bis[bis$Source.Arrow != "",]
 # and clean
-edges = data.frame("id" = edges$Id,
-                  "from" = edges$Line.Source,
+edges = data.frame("from" = edges$Line.Source,
                   "to" = edges$Line.Destination,
+                  "id" = edges$Id,
                   "type" = edges$Name,
                   stringsAsFactors = FALSE)
+
+# save ####
+write.csv(nodes, "./data/nodes.csv", row.names = FALSE)
+write.csv(edges, "./data/edges.csv", row.names = FALSE)
